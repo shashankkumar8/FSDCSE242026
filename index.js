9085e7e7d3f77640c0bfa347e63d8d09d26712db
shashankkumar8 <harishkumar99057@gmail.com>
2026-08-18 09:21:57 +0530

    Update index.js with setTimeout example

--- index.js
+++ index.js
@@ -48,3 +48,10 @@ function loginhandler(username,password,clbk){
     }
 }
 loginhandler("shashank","2344",login);
+
+console.log("one");
+for(i=0;i<100000;i++){
+    console.log("i: "+i);
+}
+//setTimeout(()=>{console.log("two")},1000);
+console.log("three");
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+console.log("one");
+for(i=0;i<100000;i++){
+    console.log("i: "+i);
+}
+//setTimeout(()=>{console.log("two")},1000);
+console.log("three");
